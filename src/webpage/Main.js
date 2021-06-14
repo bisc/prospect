6c97876cbb5a300c939d96a2ed366d056e39f404
aismaiel <alanismaiel@yahoo.com>
2021-06-13 23:55:10 -0700

    Update Web App
    The web application is now functional in the following loop:
    - On the main page, there is an input text block where PROSPECT specifications can be written
    - Clicking the submit button, the script will run and an HTML page will be displayed with the specifications
    
    TODO:
    - Fix the display of the output data, each result should be on a separate line
    - Make the website look better, include instructions on the page

--- src/webpage/Main.js
+++ src/webpage/Main.js
@@ -48,62 +48,75 @@ app.post('/run',function(req,res){
    // console.log(allText);
    console.log(allText);
 
-   fs.writeFile("demo.txt", allText, function(err, result) {
+   fs.writeFileSync("demo.txt", allText, function(err, result) {
     if(err) console.log('error', err);
    });
 
-  var part1 = "<!DOCTYPE html> \
-  <html> \
-  <body> \
-  \
-  <h1>Running Headline</h1> \
-  <p>";
+   var command = './PROSPECT.wls demo.txt'
+   var output_text = shell.exec(command)
 
-  var part2 = "</p> \
-  <p><a href="/">Return to home</a></p> \
- \
-  </body> \
-  </html>";
+  const part1 = "<!DOCTYPE html> \r <html> \r <body> \r <h1>Running Headline</h1> \r <p>";
 
-  var all_html = part1.concat(part2);
+//   const part2 = "</p> \
+//   <p><a href="/">Return to home</a></p> \
+//  \
+//   </body> \
+//   </html>";
+
+  const part2 = "</p> \r <p><a href=\"/\">Return to home</a></p> \r </body> \r </html>";
+
+  // const all_html_m = part1.concat(allText);
+  const all_html_m = part1.concat(output_text);
+
+  const all_html = all_html_m.concat(part2);
+
+
+//   const all_html = part1_m.concat("SOMETIHNG", "</p> \
+//   <p><a href="/">Return to home</a></p> \
+//  \
+//   </body> \
+//   </html>");
+//   // var all_html = part1 + part2;
 
    // Run PROSPECT
-   fs.writeFile("HTML/run_custom.html", all_html, function(err, result) {
+   fs.writeFileSync("HTML/run.html", all_html, function(err, result) {
     if(err) console.log('error', err);
    });
 
    res.sendFile('HTML/run.html', { root: __dirname });
+   // res.sendFile('HTML/run_custom.html', { root: __dirname });
    // when calling this, needs to send output of prospect to HTML
    // Make a custom HTML to do this
 });
 
 // not in use right now
 app.get('/run', function(req, res) {
-    res.sendFile('HTML/run.html', { root: __dirname });
-    // exec("ls", (error, stdout, stderr) => {
-    //     if (error) {
-    //         console.log(`error: ${error.message}`);
-    //         return;
-    //     }
-    //     if (stderr) {
-    //         console.log(`stderr: ${stderr}`);
-    //         return;
-    //     }
-    //     console.log(`stdout: ${stdout}`);
+    // res.sendFile('HTML/run.html', { root: __dirname });
+    // // exec("ls", (error, stdout, stderr) => {
+    // //     if (error) {
+    // //         console.log(`error: ${error.message}`);
+    // //         return;
+    // //     }
+    // //     if (stderr) {
+    // //         console.log(`stderr: ${stderr}`);
+    // //         return;
+    // //     }
+    // //     console.log(`stdout: ${stdout}`);
+    // // });
+
+    // var filename = req.query.f;
+    // if (!filename) {
+    //   filename = 'StaticEx';
+    // }
+    // var command = './PROSPECT.wls testing/static/' + filename + '.txt'
+    // var output_text = shell.exec(command)
+
+    // // Writing it to a file
+    // fs.writeFile("demo.txt", output_text, function(err, result) {
+    //  if(err) console.log('error', err);
     // });
-
-    var filename = req.query.f;
-    if (!filename) {
-      filename = 'StaticEx';
-    }
-    var command = './PROSPECT.wls testing/static/' + filename + '.txt'
-    var output_text = shell.exec(command)
-
-    // Writing it to a file
-    fs.writeFile("demo.txt", output_text, function(err, result) {
-     if(err) console.log('error', err);
-    });
-
+    res.sendFile('HTML/run_custom.html', { root: __dirname });
+    
 });
 
 app.listen(port, () => {
